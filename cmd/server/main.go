c5b5718b4a53b058d4d9a091d3360f2c77d5ddfb
Egor Chernakov <chernakov.eg@gmail.com>
2025-01-23 16:59:43 +0300

    init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
+
+const CONN = "amqp://guest:guest@localhost:5672/"
 
 func main() {
+	conn, err := amqp.Dial(CONN)
+	if err != nil {
+		fmt.Printf("could not connect to server: %v", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	fmt.Println("Connection successfull")
 	fmt.Println("Starting Peril server...")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down...")
 }
